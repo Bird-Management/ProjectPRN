9622d5cca463ae0a797c5aaa20bca38be5eb2ec8
MTT18 <108408243+MTT18@users.noreply.github.com>
2023-06-13 07:52:22 +0700

    Add files via upload

---
+++ Respository/Services/AccountServices.cs
@@ -0,0 +1,103 @@
+﻿using Respository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Respository.Services
+{
+    public class AccountServices
+    {
+        private readonly BirdManagementContext _context;
+
+        public AccountServices(BirdManagementContext context)
+        {
+            _context = context;
+        }
+
+        public Account LoginAccount(string username, string password)
+        {
+            try
+            {
+                var checkLogin = this._context.Account.Where(x => x.User.Equals(username) && x.Pass.Equals(password)).FirstOrDefault();
+                if (checkLogin == null)
+                {
+                    throw new Exception("Invalid account");
+                }
+
+                return checkLogin;
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<Account> GetAccounts()
+        {
+            try
+            {
+                var listAccounts = this._context.Account.Select(x => new Account { 
+                    User = x.User, 
+                    Pass = x.Pass, 
+                    IsAdmin = x.IsAdmin, 
+                    IsSell = x.IsSell,
+                    UId = x.UId
+                })
+                .ToList();
+                if (listAccounts != null)
+                {
+                    return listAccounts;
+                }
+                return null;
+
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Account NewAccountAdmin( string username, string password)
+        {
+            try
+            {
+                var account = new Account();
+                account.User = username;
+                account.Pass = password;
+                account.IsAdmin = 1;
+                account.IsSell = 0;
+
+                this._context.Add(account);
+                this._context.SaveChanges();
+
+
+                return account;
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Account NewAccountSeller(string username, string password)
+        {
+            try
+            {
+                var account = new Account();
+                account.User = username;
+                account.Pass = password;
+                account.IsAdmin = 0;
+                account.IsSell = 1;
+
+                this._context.Add(account);
+                this._context.SaveChanges();
+
+
+                return account;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ Respository/Services/ProductServices.cs
@@ -0,0 +1,38 @@
+﻿using Respository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Respository.Services
+{
+    public class ProductServices
+    {
+        private readonly BirdManagementContext _context;
+
+        public ProductServices(BirdManagementContext context)
+        {
+            _context = context;
+        }
+
+        public List<Product> GetProducts()
+        {
+            try
+            {
+                var listAllProduct = this._context.Product.ToList();
+                if (listAllProduct != null )
+                {
+                    return listAllProduct;
+                }
+                else
+                {
+                    return null;
+                }
+            }catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
