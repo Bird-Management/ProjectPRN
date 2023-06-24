b2eca043ae586e7a1b287500b1179fa52382ff0e
MTT18 <lammjnhphong4560@gmail.com>
2023-06-24 22:16:22 +0700

    update FoodServices

--- Respository/Services/FoodServices.cs
+++ Respository/Services/FoodServices.cs
@@ -1,8 +1,10 @@
 ﻿using Respository.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Repository.Services
@@ -16,24 +18,51 @@ namespace Repository.Services
             _context = context;
         }
 
-        public List<Food> GetFoods()
+        //public List<Food> GetFoods()
+        //{
+        //    try
+        //    {
+        //        var foodList = this._context.Food.ToList();
+        //        if (foodList != null)
+        //        {
+        //            return foodList;
+        //        }
+        //        else
+        //        {
+        //            return null;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new Exception(ex.Message);
+        //    }
+        //}
+
+        public List<Food> GetFoodsOther()
         {
             try
             {
-                var foodList = this._context.Food.ToList();
-                if (foodList != null)
+                var check = this._context.Food.Select(x => new Food
                 {
-                    return foodList;
-                }
-                else
+                    FoodId = x.FoodId,
+                    FoodName = x.FoodName,
+                    StartDate = x.StartDate,
+                    EndDate = x.EndDate,
+                    Producer = x.Producer,
+                    Amount = x.Amount,
+                    Price = x.Price,
+                    Status = x.Status,
+                })
+                .ToList();
+
+                if(check != null)
                 {
-                    return null;
-                }
-            }
-            catch (Exception ex)
+                    return check;
+                } return null;
+            }catch (Exception ex)
             {
                 throw new Exception(ex.Message);
-            }   
+            }
         }
 
         public List<Producer> GetProducerList()
@@ -79,5 +108,69 @@ namespace Repository.Services
                 throw new Exception(ex.Message);
             }
         }
+
+        public bool IsValidFoodID(string fID)
+        {
+            string pattern = @"^F\d{4}$";
+            return Regex.IsMatch(fID, pattern);
+
+        }
+
+        public bool FoodIdIsExist(string fID)
+        {
+            return _context.Food.Any(x => x.FoodId == fID);
+        }
+
+        public bool IsValidPriceVND(float price)
+        {
+            string pricePattern = @"^([1-9]\d{3,}|[1-9]\d{2,}\d*VND)$"; // Định dạng giá cả: lớn hơn 1000 hoặc có dạng "số lượng dương + VND"
+            string priceString = price.ToString(); // Chuyển đổi giá trị price thành chuỗi
+            return Regex.IsMatch(priceString, pricePattern);
+        }
+
+
+        public bool DeleteFood(string foodID)
+        {
+            try
+            {
+                var check = this._context.Food.FirstOrDefault(x => x.FoodId == foodID);
+                if (check != null)
+                {
+                    _context.Food.Remove(check);
+                    _context.SaveChanges();
+                    return true;
+                }
+                return false;
+            }catch (Exception ex)
+            {
+                throw new Exception (ex.Message);
+            }
+        }
+
+        public void UpdateFood(string foodID, string foodName, DateTime startDate, DateTime endDate, 
+                                    string producer, float price, int amount, bool status)
+        {
+            try
+            {
+                var foodToUpdate = _context.Food.FirstOrDefault(x => x.FoodId == foodID);
+                if (foodToUpdate != null)
+                {
+                    foodToUpdate.FoodName = foodName;
+                    foodToUpdate.StartDate = startDate;
+                    foodToUpdate.EndDate = endDate;
+                    foodToUpdate.Producer = producer;
+                    foodToUpdate.Price = price;
+                    foodToUpdate.Amount = amount;
+                    foodToUpdate.Status = status;
+
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
     }
 }
